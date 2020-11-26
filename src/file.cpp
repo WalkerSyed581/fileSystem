e80af60065ff9705c105139b0115e2639c3b21d0
SajeelHassan <sajeelh581@gmail.com>
2020-11-26 17:13:16 +0500

    Finalized the structure

---
+++ src/file.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <file.h>
+
+
+File::getData(){
+    return this->data;
+}
+
+File::write_to_file(string text){
+
+}
+
+// File::write_to_file(int write_at,string text){
+
+// }
+
+File::Read_from_file(){
+
+}
+
+// File::Read_from_file(int start,int size){
+
+// }
+
---
+++ src/file.h
@@ -0,0 +1,9 @@
+#include <string>
+
+class File {
+    private:
+        string data;
+    
+    public:
+        string name;
+}
